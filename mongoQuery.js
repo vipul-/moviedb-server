11f01ae4fc9264fae863abbaaa2f50dc8e15e6bc
Vipul Chaudhary <vpul.chaudhary@gmail.com>
2019-05-18 15:57:00 +0545

    Exported queries to an external file

---
+++ mongoQuery.js
@@ -0,0 +1,48 @@
+require('dotenv').config({
+    path: 'variables.env'
+});
+const mongoose = require('mongoose');
+const Movie = require("./models/movie");
+
+
+mongoose.connect(process.env.dbURI, {
+    useNewUrlParser: true
+});
+
+const upcomming = (page) => {
+    return Movie
+         .find({
+             status: {
+                 $ne: "Released"
+             },
+             releaseDate: {
+                 $gte: new Date()
+             },
+         })
+         .sort({
+             'releaseDate': 1
+         })
+         .skip(page * 5) //page starts with index 0
+         .limit(5);
+};
+
+const topRated = (page) => {
+    return Movie
+        .find({
+            status: {
+                $ne: "Released"
+            },
+        })
+        .sort({
+            'average_rating': -1
+        })
+        .skip(page * 5) //page starts with index 0
+        .limit(5);
+}
+
+
+
+module.exports = {
+    upcomming,
+    topRated
+}
